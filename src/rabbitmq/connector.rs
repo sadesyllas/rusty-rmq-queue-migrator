a4d32d40ccff656cec42d4e41e69039e28cfadd3
Spyridon Alfredos Desyllas <sadesyllas@gmail.com>
2025-09-12 12:15:23 +0300

    Implement application logic

--- src/rabbitmq/connector.rs
+++ src/rabbitmq/connector.rs
@@ -1,5 +1,6 @@
 use crate::error::AppError;
 use lapin::{Connection, ConnectionProperties};
+use urlencoding::encode;
 
 /// Establishes a connection to a RabbitMQ broker.
 pub async fn connect(
@@ -7,8 +8,10 @@ pub async fn connect(
     port: u16,
     user: &str,
     pass: &str,
+    vhost: &str,
 ) -> Result<Connection, AppError> {
-    let addr = format!("amqp://{}:{}@{}:{}", user, pass, host, port);
+    let encoded_vhost = encode(vhost);
+    let addr = format!("amqp://{}:{}@{}:{}/{}", user, pass, host, port, encoded_vhost);
     let conn = Connection::connect(&addr, ConnectionProperties::default()).await?;
     Ok(conn)
 }
